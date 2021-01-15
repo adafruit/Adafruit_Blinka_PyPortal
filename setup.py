b115ef5adf1d7a39456b5ad2e379ec3953bd416f
Melissa LeBlanc-Williams <melissa@adafruit.com>
2021-01-15 13:42:24 -0800

    Add PortalBase as requirement

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     install_requires=[
         "Adafruit-Blinka",
         "adafruit-blinka-displayio",
-        "adafruit-circuitpython-ili9341",
+        "adafruit-circuitpython-portalbase" "adafruit-circuitpython-ili9341",
         "adafruit-circuitpython-stmpe610",
         "adafruit-circuitpython-focaltouch",
         "adafruit-circuitpython-bitmap-font",
